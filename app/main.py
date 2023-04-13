eda24af9629002e9d3f902b50ddf105e3b918aab
billy.goo <sangmo.gu@gmail.com>
2023-04-13 21:38:37 +0900

    Create demo app

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+variant = os.environ.get("VARIANT", "DEMO")
+
+@app.route("/")
+def hello():
+    return f"Hello from Python! ({variant})"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
