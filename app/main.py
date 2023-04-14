31c88b343e126091540ca44c2c60d796ab818cbc
Billy Billy <sangmo.gu@gmail.com>
2023-04-14 10:09:11 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ variant = os.environ.get("VARIANT", "DEMO")
 
 @app.route("/")
 def hello():
-    return f"Hello from Python! ({variant})"
+    return f"Hello from Python!! ({variant})"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
